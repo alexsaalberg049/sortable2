52f7ab33e244882ac46fb2af49231ccff062e700
root <root@JHamo-HP.propro.com>
2020-03-12 12:21:44 -0700

    Initial

---
+++ example.js
@@ -0,0 +1,12 @@
+﻿
+//import * as Sortable from "./Sortable.min.js";
+//import * as Sortable from "https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js";
+import Sortable from 'sortablejs';
+
+// global export
+window.Sortable = Sortable;
+
+Sortable.create(document.getElementById("section-palette"), {
+    group: 'shared',/* options */ });
+Sortable.create(document.getElementById("section-palette"), {
+    group: 'shared',/* options */ });
